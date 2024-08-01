a240ed18567495287e392c42f81f4a909950dfe0
shashwatshukla11 <167205351+shashwatshukla11@users.noreply.github.com>
2024-08-01 18:37:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,8 +46,7 @@ async function getsongs(folder) {
     // TO SHOW SITE IN LOCAL HOST
     // let a = await fetch(`http://127.0.0.1:1000/${folder}/`)
     
-    // let a = await fetch(`/${folder}/`)
-    let a = await fetch(`${folder}`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
     // console.log(response);
     let div = document.createElement("div")
